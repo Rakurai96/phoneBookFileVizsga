304086e3c414c3e3550efa18f1d954e3834af4bd
Bajnóczi István <balbianello96@gmail.com>
2021-04-29 12:35:55 +0200

    Phonebook is completed, all tests passed.

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,28 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) throws  IllegalArgumentException {
+        if(contacts == null) {
+            throw new IllegalArgumentException();
+        }
+        if(output == null) {
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            FileWriter fileWriter = new FileWriter(output);
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                fileWriter.write(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+    }
+}
